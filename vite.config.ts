7bfd512ce144a29090c4035aa637dbf9278a0359
Gregory Huber <greg@ruboss.com>
2023-11-07 09:11:01 -0800

    add dotenv and change up some hmr stuff

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  server: {
-    hmr: false,
-  },
   plugins: [remix(), tsconfigPaths()],
 });
