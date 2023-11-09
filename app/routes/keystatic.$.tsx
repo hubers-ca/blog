abf370d1e57b6216363a12b35c54f8349885fb72
Gregory Huber <greg@ruboss.com>
2023-11-09 15:36:05 -0800

    fix env stuff

--- app/routes/keystatic.$.tsx
+++ app/routes/keystatic.$.tsx
@@ -1,4 +1,9 @@
 import { makePage } from "@keystatic/remix/ui";
 import config from "../../keystatic.config";
 
+export const loader = () => {
+  console.log(process.env["KEYSTATIC_GITHUB_CLIENT_ID"]);
+  return null;
+};
+
 export default makePage(config);
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,37 @@
 import { unstable_vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
+import { type ConfigEnv, defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import morgan from "morgan";
 
-export default defineConfig({
-  plugins: [
-    {
-      name: "morgan-logs",
-      configureServer(server) {
-        return () => server.middlewares.use(morgan("dev"));
+// This is what is wanted, there's a bug around ENV atm.
+// export default defineConfig({
+//   plugins: [
+//     {
+//       name: "morgan-logs",
+//       configureServer(server) {
+//         return () => server.middlewares.use(morgan("dev"));
+//       },
+//     },
+//     remix(),
+//     tsconfigPaths(),
+//   ],
+// });
+export default ({ mode }: ConfigEnv) => {
+  // Here we add env vars from .env files to process.env.
+  // Note the last arg is a blank string so that all env vars
+  // are loaded, not just those starting with "VITE_"
+  Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
+
+  return defineConfig({
+    plugins: [
+      {
+        name: "morgan-logs",
+        configureServer(server) {
+          return () => server.middlewares.use(morgan("dev"));
+        },
       },
-    },
-    remix(),
-    tsconfigPaths(),
-  ],
-});
+      remix(),
+      tsconfigPaths(),
+    ],
+  });
+};
