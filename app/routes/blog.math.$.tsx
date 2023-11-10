23b6be2d452b4304836a709030fb41ba053a3619
Gregory Huber <greg@ruboss.com>
2023-11-09 16:24:28 -0800

    try to use the keystatic content

---
+++ app/routes/blog.math.$.tsx
@@ -0,0 +1,28 @@
+import { createReader } from "@keystatic/core/reader";
+import { DocumentRenderer } from "@keystatic/core/renderer";
+import { json, type LoaderFunctionArgs } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import keystaticConfig from "keystatic.config";
+
+export async function loader({ params }: LoaderFunctionArgs) {
+  const reader = createReader(process.cwd(), keystaticConfig);
+  const slug = params.slug;
+  if (!slug) throw json("Not Found", { status: 404 });
+
+  const post = await reader.collections.math.read(slug, {
+    resolveLinkedFiles: true,
+  });
+  if (!post) throw json("Not Found", { status: 404 });
+
+  return json({ post });
+}
+
+export default function Post() {
+  const { post } = useLoaderData<typeof loader>();
+  return (
+    <>
+      <h1>{post.title}</h1>
+      <DocumentRenderer document={post.content} />
+    </>
+  );
+}
--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -1,9 +1,46 @@
-import { Outlet } from "@remix-run/react";
+import { Link, Outlet, useLoaderData } from "@remix-run/react";
 import { BlogSidebar } from "~/components/blog_sidebar";
+import { createReader } from "@keystatic/core/reader";
+import config from "../../keystatic.config";
+import { json } from "@remix-run/node";
+
+const loader = async () => {
+  const reader = createReader(process.cwd(), config);
+  const mathPosts = await reader.collections.math.all();
+  const codePosts = await reader.collections.code.all();
+  // console.log(mathPosts, codePosts);
+  // console.log(process.env);
+
+  return json({
+    mathPosts,
+    codePosts,
+  });
+};
 
 const BlogsLayout = () => {
+  const { mathPosts, codePosts } = useLoaderData<typeof loader>();
+  // console.log(mathPosts, codePosts);
+
   return (
     <div className="flex flex-row">
+      <ul>
+        <li>
+          <h5>Math</h5>
+        </li>
+        {mathPosts.map((post) => (
+          <li key={post.slug}>
+            <Link to={`/posts/${post.slug}`}>{post.entry.title}</Link>
+          </li>
+        ))}
+        <li>
+          <h5>Code</h5>
+        </li>
+        {codePosts.map((post) => (
+          <li key={post.slug}>
+            <Link to={`/posts/${post.slug}`}>{post.entry.title}</Link>
+          </li>
+        ))}
+      </ul>
       <BlogSidebar />
       <div className="prose prose-lg prose-neutral border-l border-neutral-300 w-full p-6 max-w-4xl">
         <Outlet />
@@ -12,4 +49,5 @@ const BlogsLayout = () => {
   );
 };
 
+export { loader };
 export default BlogsLayout;
