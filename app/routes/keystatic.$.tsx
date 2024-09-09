837d3d9b5a81c0ab1248bcc50706a54d83a794c6
Greg Huber <greg@ruboss.com>
2024-09-09 13:14:52 -0700

    add shadcn

--- app/routes/keystatic.$.tsx
+++ app/routes/keystatic.$.tsx
@@ -1,9 +1,4 @@
 import { makePage } from "@keystatic/remix/ui";
 import config from "../../keystatic.config";
 
-export const loader = () => {
-  console.log(process.env["KEYSTATIC_GITHUB_CLIENT_ID"]);
-  return null;
-};
-
 export default makePage(config);
